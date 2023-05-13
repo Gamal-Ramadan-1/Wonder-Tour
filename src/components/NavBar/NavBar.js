6bd1ef6a5cf44e3fb1fdd94c06cab67bc5f44c33
Gamal Ramadan <gamal.ramadan.fe@gmail.com>
2023-05-13 14:51:09 +0300

    Remove Tailwind Css And Add Bootstrap Css && Create NavBar And Footer Fills

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './NavBar.scss'
+const NavBar = () => {
+    return (
+        <div>
+            <h1>NavBar</h1>
+        </div>
+    )
+}
+
+export default NavBar
